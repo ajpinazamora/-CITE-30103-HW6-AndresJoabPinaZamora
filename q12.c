8feab031ffdc18cae9e62479c92db3d5b7437f6c
ajpinazamora <ajpinazamora@gmail.com>
2025-11-10 11:15:17 -0600

    Full Answer question 12 part 1

--- q12.c
+++ q12.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
 
 int main(){
-	int a = 5, b = 3, res;
+	int a = 5, b = 3, res, res2;
 
 	res = a + (b<<1);
 
-	printf("Result: %d\n", res);
-	
+	res2 = a+b*2;
+
+	printf("Result using parenthesis: %d\n", res);
+	printf("Result using arithmetic: %d\n", res2);
 	return 0;
 }
